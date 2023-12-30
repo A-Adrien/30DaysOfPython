65dfd1dc9e494dbc4959ffdaf1c47548d953fe37
adrien <astadrien21@gmail.com>
2023-12-30 13:07:10 +0100

    day_1

---
+++ day_1/helloworld.py
@@ -0,0 +1,38 @@
+import sys
+import math
+
+print(sys.version)
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(4 % 3)
+print(4 / 3)
+print (3 ** 4)
+print(4 // 3)
+
+print('Adrien')
+print('A')
+print('France')
+print('I am enjoying the 30 days of python')
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Adrien'))
+print(type('A'))
+print(type('France'))
+
+print(21) #integer
+print(2.1) #float
+print(2-1j) #complex
+print('This is a string.') #string
+print(True) #boolean
+print([1, 3, 4, 2, 2]) #list
+print((2, 3, 1, 4)) #tuple
+print({1, 3, 2, 4}) #set
+print({'name':'Adrien'}) #dictionary
+
+print(math.dist([2,3], [10,8]))
